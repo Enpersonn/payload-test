39a6cea03649b21655d56395d2279ae10d766131
Enpersonn <nicklas.b.overjordet@gmail.com>
2025-01-27 21:31:20 +0100

    hide data from payload on homepage

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,11 +1,11 @@
 import HomePage from "@/pages/home.page";
-import { getPayload } from "payload";
-import config from "@payload-config";
+// import { getPayload } from "payload";
+// import config from "@payload-config";
 
 export default async function Page() {
-  const payload = await getPayload({ config });
-  const albums = await payload.find({
-    collection: "albums",
-  });
-  return <pre>{JSON.stringify(albums, null, 2)}</pre>;
+  // const payload = await getPayload({ config });
+  // const albums = await payload.find({
+  //   collection: "albums",
+  // });
+  return <HomePage />;
 }
